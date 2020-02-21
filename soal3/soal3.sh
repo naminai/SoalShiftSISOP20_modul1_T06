f8896c6baafec350884eb6e0bc8022981bc4f4f7
Donny K R <61267430+naminai@users.noreply.github.com>
2020-02-21 14:37:44 +0700

    Update soal3.sh

--- soal3/soal3.sh
+++ soal3/soal3.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+display=`pwd`
+namber=`ls $display | grep "pdkt_kusuma" | cut -d "_" -f 3 |sort -n | tail -1`
+if [[ $namber =~ [0..9] ]]
+then
+  namber=0
+fi
+a=`expr $namber + 1` 
+b=`expr $namber + 28`
+for ((c=x; c<=y; c++ ))
+do
+     wget -a $display/wget.log -O "$display/pdkt_kusuma_$c" https://loremflickr.com/320/240/  
+done
+grep "Location" wget.log >> location.log
