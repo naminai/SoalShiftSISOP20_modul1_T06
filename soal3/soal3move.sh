1d21be3ccf47716ef22958cc1beb312e450844f1
Donny K R <61267430+naminai@users.noreply.github.com>
2020-02-21 15:00:39 +0700

    wget.log.bak

--- soal3/soal3move.sh
+++ soal3/soal3move.sh
@@ -1,37 +1,32 @@
 #!/bin/bash
-dirinfo=`pwd`
-testering=`ls $dirinfo | grep "pdkt_kusuma" | cut -d "_" -f 3 | sort -n | tail -1`
+display=`pwd`
+testering=`ls | grep "pdkt_kusuma" | cut -d "_" -f 3 | sort -n | tail -1`
 
-if [[ `ls $dirinfo | grep "kenangan"` != "kenangan" ]]
+x=`mkdir $display/kenangan`
+y=`mkdir $display/duplicate`
+
+if [[ `ls $display | grep "kenangan"` != "kenangan" ]]
 then
-  mkdir $dirinfo/kenangan
+  $x
 fi
 
-if [[ `ls $dirinfo | grep "duplicate"` != "duplicate" ]]
+if [[ `ls $display | grep "duplicate"` != "duplicate" ]]
 then
-  mkdir $dirinfo/duplicate
+  $y
 fi
 
 arr=""
 for ((i=1; i<=$testering; i++))
 do
-  loc="`cat $dirinfo/wget.log | grep "Location:" | head -$i | tail -1 | cut -d " " -f 2`"
-  isDuplicate=`echo -e $arr | awk -v loc=$loc 'BEGIN {isDuplicate=0} {if (loc==$0) isDuplicate=1} END {printf "%d", isDuplicate}'`
-  if [[ $isDuplicate == 1 ]]
+  lokasi="`cat $display/wget.log | grep "Location" | head -$i | tail -1 | cut -d " " -f 2`"
+  dupes=`echo -e $arr | awk -v lokasi=$lokasi 'BEGIN {dupes=0} {if (lokasi==$0) dupes=1} END {printf "%d", dupes}'`
+  if [[ $dupes == 1 ]]
   then
-    mv $dirinfo/pdkt_kusuma_$i $dirinfo/duplicate/duplicate_$i
+    mv $display/pdkt_kusuma_$i $display/duplicate/duplicate_$i
   else
-    arr="$arr$loc\n"
-    mv $dirinfo/pdkt_kusuma_$i $dirinfo/kenangan/kenangan_$i
+    arr="$arr$lokasi\n"
+    mv $display/pdkt_kusuma_$i $display/kenangan/kenangan_$i
   fi
 done
 
 cat $dirinfo/wget.log >> $dirinfo/wget.log.bak && rm $dirinfo/wget.log # && rm location.log
-#if [[ $testering =~ [0..9] ]]
-#then
-#  testering=0
-#fi
-
-#x=`expr $testering + 1`
-#y=`expr $testering + 28`
-
