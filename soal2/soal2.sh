96f33fc68bd172df5370070785799a74dd48a2e3
Donny K R <61267430+naminai@users.noreply.github.com>
2020-02-29 12:22:20 +0700

    revisi soal 2a

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 Nama=$*
-Randomizer=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 28 | head -n 1)
+#!/bin/bash
+
+Nama=$*
+Randomizer=`pwgen -c -n 28 1`
+echo $Randomizer
+Kode=`printf '%s\n' "${Nama//[[:digit:]]/}"`
+echo $Randomizer >> $Kode.txt 
+
 echo $Randomizer
 Kode=`printf '%s\n' "${Nama//[[:digit:]]/}"`
 echo $Randomizer >> $Kode.txt 
