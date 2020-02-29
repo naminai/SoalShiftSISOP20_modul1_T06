238bcea8a1263db497f44f2f02b5220652ef2a9d
Donny K R <61267430+naminai@users.noreply.github.com>
2020-02-29 12:23:08 +0700

    Update soal2.sh

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 Nama=$*
-#!/bin/bash
-
-Nama=$*
 Randomizer=`pwgen -c -n 28 1`
-echo $Randomizer
 Kode=`printf '%s\n' "${Nama//[[:digit:]]/}"`
-echo $Randomizer >> $Kode.txt 
-
-echo $Randomizer
-Kode=`printf '%s\n' "${Nama//[[:digit:]]/}"`
-echo $Randomizer >> $Kode.txt 
+echo $Randomizer >> $Kode.txt
