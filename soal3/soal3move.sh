b453b7981397e002ad338619ed1c7af3729b85ad
Donny K R <61267430+naminai@users.noreply.github.com>
2020-02-29 12:29:27 +0700

    soal 3 revisi

--- soal3/soal3move.sh
+++ soal3/soal3move.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
-display=`pwd`
-testering=`ls | grep "pdkt_kusuma" | cut -d "_" -f 3 | sort -n | tail -1`
+dirinfo=`pwd`
+testering=`ls $dirinfo | grep "pdkt_kusuma" | cut -d "_" -f 3 | sort -n | tail -1`
 
-x=`mkdir $display/kenangan`
-y=`mkdir $display/duplicate`
+mkdir $dirinfo/kenangan
+mkdir $dirinfo/duplicate
 
-if [[ `ls $display | grep "kenangan"` != "kenangan" ]]
-then
-  $x
-fi
-
-if [[ `ls $display | grep "duplicate"` != "duplicate" ]]
-then
-  $y
-fi
-
+x=0
+y=0
 arr=""
 for ((i=1; i<=$testering; i++))
 do
-  lokasi="`cat $display/wget.log | grep "Location" | head -$i | tail -1 | cut -d " " -f 2`"
+  lokasi="`cat $dirinfo/wget.log | grep "Location:" | head -$i | tail -1 | cut -d " " -f 2`"
   dupes=`echo -e $arr | awk -v lokasi=$lokasi 'BEGIN {dupes=0} {if (lokasi==$0) dupes=1} END {printf "%d", dupes}'`
   if [[ $dupes == 1 ]]
   then
-    mv $display/pdkt_kusuma_$i $display/duplicate/duplicate_$i
+  x=`expr $x + 1`
+      mv $dirinfo/pdkt_kusuma_$i $dirinfo/duplicate/duplicate_$x
   else
-    arr="$arr$lokasi\n"
-    mv $display/pdkt_kusuma_$i $display/kenangan/kenangan_$i
+  arr="$arr$lokasi\n"
+  y=`expr $y + 1`
+    mv $dirinfo/pdkt_kusuma_$i $dirinfo/kenangan/kenangan_$y
   fi
 done
 
-cat $dirinfo/wget.log >> $dirinfo/wget.log.bak && rm $dirinfo/wget.log # && rm location.log
+cat $dirinfo/wget.log >> $dirinfo/wget.log.bak && rm $dirinfo/wget.log 
